5b527dafb89349d597e9362744058a12b6ef6026
Alex Duchesne <alex@netvps.ca>
2023-03-17 16:56:52 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,36 @@
+# Description
+
+This project is an implementation of the restic's [REST backend API](https://restic.readthedocs.io/en/latest/100_references.html#rest-backend) in PHP. It is a replacement for [restic/rest-server](https://github.com/restic/rest-server).
+
+|Feature        |restic/rest-server|restic-rest-php|
+|---------------|-----------|---------------|
+| Auth          | Yes (.htpasswd) | Yes (web server) |
+| TLS           | Yes       | Yes (web server) |
+| Append only   | Yes       | Yes            |
+| Private repos | Yes       | Yes            |
+| Metrics       | Yes       | No             |
+
+
+# Usage
+
+Change `DATA_DIR` in the script to the absolute path of where you want the repositories to live. 
+It might be a good idea to put `DATA_DIR` outside of the web root but it's not obligatory.
+
+NGINX:
+````
+location / {
+    rewrite ^(.*)$ /index.php?$1 last;
+}
+````
+
+Apache:
+````
+RewriteEngine On
+RewriteRule ^.*$ index.php [L]
+````
+
+Testing (set NO_AUTH to true):
+````
+$ php -S 127.0.0.1:8000 index.php
+$ restic -r rest:http://127.0.0.1:8000/ init
+````
---
+++ index.php
@@ -0,0 +1,125 @@
+<?php
+/**
+ * This project implements the restic REST API described at:
+ * https://restic.readthedocs.io/en/latest/100_references.html#rest-backend
+ */
+define('OBJECT_TYPES', ['data', 'keys', 'locks', 'snapshots', 'index']);
+define('FILE_TYPES', ['config']);
+
+define('APPEND_ONLY', false);   // --append-only
+define('NO_AUTH', false);       // --no-auth
+define('PRIVATE_REPOS', false); // --private-repos
+define('DATA_DIR', './restic'); // --path
+
+[$method, $user, $path, $uri, $query] = parse_request($_SERVER);
+
+if (empty($user) && (PRIVATE_REPOS || !NO_AUTH)) {
+    respond(401, [], 'You must be logged in.');
+}
+
+if (preg_match('~^(' . implode('|', OBJECT_TYPES) . ')/([a-zA-Z0-9]{64})$~', $uri) || in_array($uri, FILE_TYPES)) {
+    handle_file($method, joinpath($path, $uri));
+} elseif (in_array($uri, OBJECT_TYPES)) {
+    handle_list($method, joinpath($path, $uri));
+} elseif ($uri === '') {
+    handle_repo($method, $path, $query);
+} else {
+    respond(400, [], 'Bad Request');
+}
+
+
+function parse_request(array $server)
+{
+    $method = strtoupper($server['REQUEST_METHOD']);
+    $user = $server['REMOTE_USER'] ?? '';
+    $find = implode('|', array_merge(OBJECT_TYPES, FILE_TYPES));
+    if (preg_match("~^(?<p>.*?)(?<u>/($find)(/.*)?|/)(\?(?<q>.*))?$~", $server['REQUEST_URI'], $m)) {
+        $path = PRIVATE_REPOS ? joinpath(DATA_DIR, $user, $m['p']) : joinpath(DATA_DIR, $m['p']);
+        return [$method, $user, $path, trim($m['u'], '/'), $m['q'] ?? ''];
+    }
+    return [$method, $user, '', '', ''];
+}
+
+function joinpath(string ...$parts)
+{
+    return rtrim(implode('/', array_filter($parts, 'strlen')), '/');
+}
+
+function respond(int $http_code = 200, array $headers = [], string $content = null)
+{
+    header('Content-Type: application/vnd.x.restic.rest.v1');
+    http_response_code($http_code); // We'll override on errors
+    foreach ($headers as $header)
+        header($header);
+    die($content);
+}
+
+// {PATH}
+function handle_repo(string $method, string $folderPath, string $query)
+{
+    if ($method === 'POST') {
+        if ($query === 'create=true') { // create repo
+            if (glob("$folderPath/*")) {
+                respond(400, [], 'Folder not empty');
+            }
+            foreach (OBJECT_TYPES as $type) {
+                mkdir(joinpath($folderPath, $type), 0755, true);
+            }
+            respond(200);
+        }
+    } elseif ($method === 'DELETE') { // delete repo
+        respond(501, [], 'Not implemented');
+    } else {
+        respond(405, [], 'Method not allowed');
+    }
+}
+
+// {PATH}/{TYPE}
+function handle_list(string $method, string $filePath)
+{
+    if ($method === 'GET') {
+        $directory = new \RecursiveDirectoryIterator($filePath, \RecursiveDirectoryIterator::SKIP_DOTS);
+        $files = [];
+        foreach (new \RecursiveIteratorIterator($directory) as $file) {
+            $files[] = $file->getBasename();
+        }
+        respond(200, [], json_encode($files));
+    } else {
+        respond(405, [], 'Method not allowed');
+    }
+}
+
+// {PATH}/{TYPE}/{NAME} and {PATH}/{NAME}
+function handle_file(string $method, string $filePath)
+{
+    if ($is_object = preg_match('/^[a-z0-9]{64}$/', basename($filePath))) {
+        $filePath = preg_replace('~/data/((..).{62})$~', '/data/$2/$1', $filePath);
+    }
+    if ($method === 'GET' || $method === 'HEAD') {
+        if (!is_file($filePath) || !is_readable($filePath)) {
+            respond(404);
+        }
+        $content = $method === 'GET' ? file_get_contents($filePath) : NULL;
+        respond(200, ['Content-Length: ' . filesize($filePath)], $content);
+    } elseif ($method === 'POST') {
+        @mkdir(dirname($filePath), 0755, true);
+        if (!@copy('php://input', $filePath)) { // FIXME: Atomic copy would be nicer...
+            respond(500);
+        } elseif ($is_object && hash_file('sha256', $filePath) !== basename($filePath)) {
+            @unlink($filePath);
+            respond(500);
+        }
+        respond(200);
+    } elseif ($method === 'DELETE') {
+        if (APPEND_ONLY) {
+            respond(403);
+        } elseif (!file_exists($filePath)) {
+            respond(404);
+        } elseif (!@unlink($filePath)) {
+            respond(500);
+        }
+        respond(200);
+    } else {
+        respond(405, [], 'Method not allowed');
+    }
+}
